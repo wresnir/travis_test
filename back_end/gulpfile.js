7cf730e8befeb20a94166809e63c5516764e5cfa
wresnir <wresni.ronggowerdhi@gmail.com>
2018-12-26 16:12:37 +0700

    guld test

---
+++ back_end/gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-csso');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('travis', ['build', 'testServerJS'], function(){
+    process.exit(0);
+});
